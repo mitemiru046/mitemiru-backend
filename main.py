24882eadfa1deefc6dc8abe6848e7043e69c16bd
mitemiru-backend <krsnkv47@gmail.com>
2025-07-12 23:06:27 +0900

    Simplify CORS settings

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 from dotenv import load_dotenv
-import os, random, requests
+import os
+import random
+import requests
 
-# .env を読み込む
 load_dotenv(Path(__file__).parent / ".env")
 TMDB_KEY = os.getenv("TMDB_KEY")
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "https://axiomatic-trip-production.up.railway.app"
+    ],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
@@ -16,7 +29,7 @@ def health():
 @app.get("/recommend")
 def recommend():
     url = (
-        f"https://api.themoviedb.org/3/trending/movie/day"
+        "https://api.themoviedb.org/3/trending/movie/day"
         f"?api_key={TMDB_KEY}&language=ja-JP"
     )
     movie = random.choice(requests.get(url, timeout=10).json()["results"])
